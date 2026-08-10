dc966ea77cdcd58b39e75347a018fb295ff85b9b
harshit-kunjadiya-07 <harshitkunjadiya52@gmail.com>
2026-08-10 10:28:05 +0530

    c++ program

---
+++ day4/oop/class.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+class Student{
+    public:
+
+    string fname , lname;
+    int age;
+    
+}
+
+int main()
+{
+    Student s1;
+    s1.fname = "John";
+    s1.lname = "Doe";
+    s1.age = 20;
+
+    cout << "Student Name: " << s1.fname << " " << s1.lname << endl;
+    cout << "Student Age: " << s1.age << endl;
+
+    return 0;
+}
